f4cc8805f3e76893be1636b3265e9e9d6e22aadc
Eva S. Poulsen <eva@btech.au.dk>
2022-04-18 15:16:15 +0200

    navigation prop 1

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,14 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 
-function HomeScreen() {
+function HomeScreen({ navigation }) {
     return (
         <View style={styles.viewStyle}>
             <Text>Home Screen</Text>
+            <Button
+                title="Go to Details"
+                onPress={() => navigation.navigate('Details')}
+            />
         </View>
     );
 }
